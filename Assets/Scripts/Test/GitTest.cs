90017850c02906f6d84e8c508e53af136698b5bc
koseungro <goseungro@naver.com>
2024-03-19 12:13:27 +0900

    Update GitTest.cs

--- Assets/Scripts/Test/GitTest.cs
+++ Assets/Scripts/Test/GitTest.cs
@@ -9,6 +9,8 @@ public class GitTest : MonoBehaviour
 
     // branch
     private string str = "Test branch";
+    public string branch = "branch";
+
     // Start is called before the first frame update
     void Start()
     {
