d9393c566348f21eb6253c28bfe55ecfd6ee25b4
koseungro <54518513+koseungro@users.noreply.github.com>
2024-03-19 12:00:59 +0900

    Update GitTest.cs

--- Assets/Scripts/Test/GitTest.cs
+++ Assets/Scripts/Test/GitTest.cs
@@ -6,6 +6,9 @@ public class GitTest : MonoBehaviour
 {
     private Vector3 vec;
 private Transform tr;
+
+// branch
+private string str;
     // Start is called before the first frame update
     void Start()
     {
